95a8f90cce5ab4d482e84c656598f6f35eb3acc1
Jakkaphan Payuhakait <tee.jakkaphan@gmail.com>
2021-02-12 19:04:27 +0700

    Add css

--- src/main.js
+++ src/main.js
@@ -6,6 +6,7 @@ import store from './store'
 import BootstrapVue from 'bootstrap-vue'
 import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap-vue/dist/bootstrap-vue.css'
+import '@/assets/styles/main.scss'
 
 Vue.use(BootstrapVue)
 
